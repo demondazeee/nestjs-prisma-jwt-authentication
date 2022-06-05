116653de2b7f24d18905b1bb3ad71941807714ec
Bryan <bryanjimavila@gmail.com>
2022-06-06 06:27:19 +0800

    imported modules and providers on tests

--- src/auth/auth.controller.spec.ts
+++ src/auth/auth.controller.spec.ts
@@ -1,11 +1,25 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { PrismaService } from '../prisma.service';
+import { UsersModule } from '../users/users.module';
+import { UsersService } from '../users/users.service';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 describe('AuthController', () => {
   let controller: AuthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports:[UsersModule, JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject:[ConfigService],
+        useFactory(config: ConfigService){
+          return config.get('JWT_KEY')
+        }
+      })],
+      providers: [ UsersService, PrismaService, AuthService],
       controllers: [AuthController],
     }).compile();
 
--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,12 +1,24 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { Test, TestingModule } from '@nestjs/testing';
+import { UsersService } from '../users/users.service';
+import { UsersModule } from '../users/users.module';
 import { AuthService } from './auth.service';
+import { PrismaService } from '../prisma.service';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [UsersModule, JwtModule.registerAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory(config:ConfigService){
+          return config.get('JWT_KEY')
+        }
+      })],
+      providers: [AuthService, UsersService, PrismaService],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
